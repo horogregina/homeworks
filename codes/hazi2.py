31da76c02b26b1f83cdba88846d6084362093752
horogregina <113914221+horogregina@users.noreply.github.com>
2022-09-24 15:35:35 +0200

    Add files via upload

---
+++ codes/hazi2.py
@@ -0,0 +1,10 @@
+def hazi2():
+    print("Adja meg a háromszög három oldalát cm-ben: ")
+    a = int(input("a oldal (cm): "))
+    b = int(input("b oldal (cm): "))
+    c = int(input("c oldal (cm): "))
+    if (a + b) > c and (a + c) > b and (b + c) > a:
+        print("A(z) " + str(a) + " és " + str(b) + ", " + str(c) + " oldalú háromszög szerkeszthető.")
+    else:
+        print("A(z) " + str(a) + " és " + str(b) + ", " + str(c) + " oldalú háromszög NEM szerkeszthető.")
+hazi2()
